9b8b74cc646f166a474cac352c204b69bf9c0fd9
e1010101 <ezratio15195@gmail.com>
2024-01-02 20:23:25 +0800

    Update works and resume

--- pages/works.js
+++ pages/works.js
@@ -1,11 +1,11 @@
 import NextLink from "next/link";
 import {
-  Container,
-  Heading,
-  Link,
-  SimpleGrid,
-  Divider,
-  useColorModeValue,
+    Container,
+    Heading,
+    Link,
+    SimpleGrid,
+    Divider,
+    useColorModeValue,
 } from "@chakra-ui/react";
 import Layout from "../components/layouts/article";
 import Section from "../components/section";
@@ -17,115 +17,132 @@ import thumbChess from "../public/images/works/chessappThumbnail.webp";
 import thumbHuman from "../public/images/works/humanThumbnail.webp";
 import thumbPasword from "../public/images/works/passwordGeneratorThumbnail.webp";
 import thumbDataAnalysis1 from "../public/images/works/dataAnalysis1Thumbnail.webp";
+import thumbStableDiffusion from "../public/images/works/stableDiffusionThumbnail.webp";
 
 const Works = () => (
-  <Layout title="Works">
-    <Container
-      m="auto"
-      maxW="container.lg"
-      textAlign="center"
-      background={useColorModeValue("#F7F9F7", "#210124")}
-      borderBottomRadius="xl"
-      p="10"
-    >
-      <Heading as="h2" fontSize={40} mt={4} mb={4}>
-        My Works
-      </Heading>
+    <Layout title="Works">
+        <Container
+            m="auto"
+            maxW="container.lg"
+            textAlign="center"
+            background={useColorModeValue("#F7F9F7", "#210124")}
+            borderBottomRadius="xl"
+            p="10"
+        >
+            <Heading as="h2" fontSize={40} mt={4} mb={4}>
+                My Works
+            </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="dinosaurDetector"
-            title="Dinosaur Detector"
-            thumbnail={thumbDinosaur}
-            link="https://dinosaur-detector.netlify.app/"
-          >
-            A simple custom-trained image recognition model. Users can upload an
-            image and the model will classify it as a dinosaur or not. The model
-            was trained with images of dinosaurs, humans, landscapes, animals
-            and vehicles.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="aiGallery"
-            title="AI Image Gallery"
-            thumbnail={thumbGallery}
-            link="https://ai-image-gallery.vercel.app/"
-          >
-            A gallery of AI-created artworks, generated from diffusive models
-            such as DALL-E 2, Midjourney and Stable Diffusion, stored in a
-            dynamic gallery with some cool features.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="humanMod"
-            title="Harold The Human"
-            thumbnail={thumbHuman}
-            link="https://steamcommunity.com/sharedfiles/filedetails/?id=2489999372"
-          >
-            A character modification for popular indie game Slay The Spire,
-            which introduces many new and interesting mechanics.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="passwordGenerator"
-            title="Password Generator"
-            thumbnail={thumbPasword}
-            link="https://ezra-password-generator.vercel.app/"
-          >
-            A random password generator with options for uppercase, numeric and
-            symbolic options.
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="dataAnalysis1"
-            title="911 Call Dataset Analysis"
-            thumbnail={thumbDataAnalysis1}
-            link="https://colab.research.google.com/drive/18I39pqDRmLb5SqvSZUI4D7iPe1pSjP1N?usp=sharing"
-          >
-            Exploratory data analysis conducted on a 911 call dataset from{" "}
-            <NextLink
-              href="https://www.kaggle.com/datasets/mchirico/montcoalert"
-              passHref
-              scroll={false}
-            >
-              <Link target="_blank" rel="noopener noreferrer">
-                Kaggle
-              </Link>
-            </NextLink>
-            . Analysis and data visualisation is conducted with pandas and
-            seaborn.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem
+                        id="dinosaurDetector"
+                        title="Dinosaur Detector"
+                        thumbnail={thumbDinosaur}
+                        link="https://dinosaur-detector.netlify.app/"
+                    >
+                        A simple custom-trained image recognition model. Users
+                        can upload an image and the model will classify it as a
+                        dinosaur or not. The model was trained with images of
+                        dinosaurs, humans, landscapes, animals and vehicles.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem
+                        id="aiGallery"
+                        title="AI Image Gallery"
+                        thumbnail={thumbGallery}
+                        link="https://ai-image-gallery.vercel.app/"
+                    >
+                        A gallery of AI-created artworks, generated from
+                        diffusive models such as DALL-E 2, Midjourney and Stable
+                        Diffusion, stored in a dynamic gallery with some cool
+                        features.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem
+                        id="humanMod"
+                        title="Harold The Human"
+                        thumbnail={thumbHuman}
+                        link="https://steamcommunity.com/sharedfiles/filedetails/?id=2489999372"
+                    >
+                        A character modification for popular indie game Slay The
+                        Spire, which introduces many new and interesting
+                        mechanics.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem
+                        id="passwordGenerator"
+                        title="Password Generator"
+                        thumbnail={thumbPasword}
+                        link="https://ezra-password-generator.vercel.app/"
+                    >
+                        A random password generator with options for uppercase,
+                        numeric and symbolic options.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem
+                        id="dataAnalysis1"
+                        title="911 Call Dataset Analysis"
+                        thumbnail={thumbDataAnalysis1}
+                        link="https://colab.research.google.com/drive/18I39pqDRmLb5SqvSZUI4D7iPe1pSjP1N?usp=sharing"
+                    >
+                        Exploratory data analysis conducted on a 911 call
+                        dataset from{" "}
+                        <NextLink
+                            href="https://www.kaggle.com/datasets/mchirico/montcoalert"
+                            passHref
+                            scroll={false}
+                        >
+                            <Link target="_blank" rel="noopener noreferrer">
+                                Kaggle
+                            </Link>
+                        </NextLink>
+                        . Analysis and data visualisation is conducted with
+                        pandas and seaborn.
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem
+                        id="stableDiffusionImplementation"
+                        title="Stable Diffusion Implementation"
+                        thumbnail={thumbStableDiffusion}
+                        link="https://colab.research.google.com/drive/1PBoeSd1Nr1CxRUPEOJ-Hu9GeWV4tKWm-?usp=sharing"
+                    >
+                        A implementation of a diffusion-based inferential
+                        pipeline, allowing a user to generate images from a text
+                        prompt. A prompt is encoded to a text embedding, which
+                        is used to guide the generation process in latent space.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
 
-      <Section delay={0.2}>
-        <Divider my={6} />
+            <Section delay={0.2}>
+                <Divider my={6} />
 
-        <Heading as="h2" fontSize={40} mt={4} mb={4}>
-          Collaborations
-        </Heading>
-      </Section>
+                <Heading as="h2" fontSize={40} mt={4} mb={4}>
+                    Collaborations
+                </Heading>
+            </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem
-            id="chessApp"
-            title="BetterChess"
-            thumbnail={thumbChess}
-            link="https://orbital-fried-liver.web.app/"
-          >
-            A website to play Chess with an emphasis on user engagement and
-            community building.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
-  </Layout>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section delay={0.3}>
+                    <WorkGridItem
+                        id="chessApp"
+                        title="BetterChess"
+                        thumbnail={thumbChess}
+                        link="https://orbital-fried-liver.web.app/"
+                    >
+                        A website to play Chess with an emphasis on user
+                        engagement and community building.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    </Layout>
 );
 
 export default Works;
